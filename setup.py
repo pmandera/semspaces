82f59b3e0beeee58c9dbd2eaa7401ea754c67e3a
Paweł Mandera <pawel@pawelmandera.com>
2018-10-29 16:06:37 +0100

    Update setup.py for pypi upload

--- setup.py
+++ setup.py
@@ -2,22 +2,32 @@
 # -*- encoding: utf-8 -*-
 from setuptools import setup, find_packages
 
-setup(
-    name = "semspaces",
-    version = "0.1.2",
-    packages = find_packages(),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-    install_requires = ['fs>=0.5.0',
-                        'numpy>=1.9.1',
-                        'scipy>=0.14.0',
-                        'pandas>=0.15.1',
-                        'scikit-learn>=0.15.0'],
+setup(
+    name="semspaces",
+    version="0.1.2",
+    packages=find_packages(),
+    scripts=['bin/w2v2ssm', 'bin/csv_sims', 'bin/subset_space'],
+
+    install_requires=['fs>=0.5.0',
+                       'numpy>=1.9.1',
+                       'scipy>=0.14.0',
+                       'pandas>=0.15.1',
+                       'scikit-learn>=0.15.0'],
+
 
     # metadata for upload to PyPI
-    author = "Paweł Mandera",
-    author_email = "pawel.mandera@ugent.be",
-    description = "This is a package for working with semantic spaces.",
-    keywords = "semantic space word vectors",
-
-    scripts = ['bin/w2v2ssm', 'bin/csv_sims', 'bin/subset_space']
+    author="Paweł Mandera",
+    author_email="pawel@pawelmandera.com",
+    description="Package for working with semantic spaces.",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/pmandera/semspaces/",
+    keywords="semantic space word vectors",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
 )
