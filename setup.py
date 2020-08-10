c5da9155e4da924a46f219cf4a2e3ac15337b757
Paweł Mandera <pawel@pawelmandera.com>
2020-08-10 13:14:41 +0200

    Improve reading and writing from/to csv files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "semspaces",
-    version = "0.1.2",
+    version = "0.1.4",
     packages = find_packages(),
 
     install_requires = ['fs>=0.5.0',
