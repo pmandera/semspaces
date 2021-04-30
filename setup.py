43fe8b4f948422e73a2eb7cc6a3f9601f730d9dc
Paweł Mandera <pawel@pawelmandera.com>
2021-04-30 12:03:14 +0200

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="semspaces",
-    version = "0.1.4",
+    version = "0.1.5",
     packages=find_packages(),
     scripts=['bin/w2v2ssm', 'bin/csv_sims', 'bin/subset_space'],
     install_requires=['fs>=0.5.0',
